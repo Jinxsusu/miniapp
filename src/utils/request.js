61e00e20a73b1426380ad1d722eada172b9b8987
Jinx <Jinx@qq.com>
2019-10-19 00:25:31 +0800

    在请求发送前后设置loading 提示框 用的是小程序的api

--- src/utils/request.js
+++ src/utils/request.js
@@ -4,13 +4,16 @@ export default function (obj) {
   let data = obj.data || {}
   url = 'https://www.uinav.com/' + url
   return new Promise((resolve, reject) => {
+    // 请求发送之前设置loading 显示loading提示框
+    mpvue.showLoading({ title: '正在加载中...' })
     mpvue.request({
       url,
       method,
       data,
       success (info) {
-        // console.log(info)
         resolve(info.data)
+        // 请求完成之后 隐藏 loading 提示框
+        mpvue.hideLoading()
       }
     })
   })
